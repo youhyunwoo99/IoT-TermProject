268d671de847d81d6d220b215b36d2316c96fd58
1109min <1109min@naver.com>
2023-05-17 19:16:13 +0900

    Design: Design MainActivity layout

--- WiFi_positioning_IoT/app/src/main/java/com/gachon/wifi_positioning_iot/MainActivity.java
+++ WiFi_positioning_IoT/app/src/main/java/com/gachon/wifi_positioning_iot/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gachon.wifi_positioning_iot;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
