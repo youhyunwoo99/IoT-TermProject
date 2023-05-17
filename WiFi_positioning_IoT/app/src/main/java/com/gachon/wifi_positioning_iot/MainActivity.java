3096faf2d10a49907133474211dca9b4a6fdebcc
1109min <1109min@naver.com>
2023-05-17 19:02:36 +0900

    FEat: make template

---
+++ WiFi_positioning_IoT/app/src/main/java/com/gachon/wifi_positioning_iot/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gachon.wifi_positioning_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
