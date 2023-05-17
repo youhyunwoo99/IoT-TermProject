5239cca324ca2ca1e986f405b80c4151ab0641e3
1109min <1109min@naver.com>
2023-05-17 19:28:10 +0900

    Design: MainActivity layout

--- WiFi_positioning_IoT/app/src/main/java/com/gachon/wifi_positioning_iot/MainActivity.java
+++ WiFi_positioning_IoT/app/src/main/java/com/gachon/wifi_positioning_iot/MainActivity.java
@@ -3,9 +3,30 @@ package com.gachon.wifi_positioning_iot;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText serverAddressInput;
+    private EditText userNameInput;
+    private String positionText;
+    private TextView resultText;
+
+    private Button addDatasetBtn;
+
+    private WifiManager wifiManager;
+
+    private String serverAddress;
+    private String URL;
+
+    JSONObject one_wifi_json = new JSONObject();
+    JSONObject result_json = new JSONObject();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        serverAddressInput = findViewById(R.id.serverAddressInput);
+        addDatasetBtn = findViewById(R.id.button);
+        positionInput = findViewById(R.id.positionInput);
+        resultText = findViewById(R.id.resultText);
     }
 }
